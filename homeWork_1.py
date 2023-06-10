afd9c0b426f4570a4420b2440a0a64f89ac0a6c4
unknown <leonidych73@gmail.com>
2023-06-10 15:08:49 -0500

    3 test commit

--- homeWork_1.py
+++ homeWork_1.py
@@ -1,5 +1,5 @@
 # homeWork_1
-#___________________________________________
+# ___________________________________________
 # we have norway text in old style formatting
 # re-write the same text as:
 # #1 string with format() call
